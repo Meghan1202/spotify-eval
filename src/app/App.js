d487a256ee77ee502a1daabdd31e9564ebacb80b
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-15 11:04:56 +0530

    feat: add NavBar Component

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import React from 'react';
+import NavBar from '../components/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
-      WELCOME
+      <NavBar />
     </div>
   );
 }
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+const NavBar = () => (
+  <div className="Nav-Bar-Container">
+    My &nbsp;
+    <strong> Record </strong>
+    &nbsp; Shelf
+  </div>
+);
+
+export default NavBar;
