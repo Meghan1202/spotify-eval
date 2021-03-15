4af9b7d0697e8c61930eadfc254554f38e2c2140
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-15 17:39:51 +0530

    fix: add test cases for Like component

--- src/components/GenreHeader/__test__/GenreHeader.test.jsx
+++ src/components/GenreHeader/__test__/GenreHeader.test.jsx
@@ -4,7 +4,7 @@ import GenreHeader from '../GenreHeader';
 
 describe(GenreHeader.name, () => {
   it('should match the snapshot', () => {
-    const { container } = render(<GenreHeader />);
+    const { container } = render(<GenreHeader Genre="Bollywood" />);
     expect(container).toMatchSnapshot();
   });
 });
--- src/components/LikeButton/__test__/LikeButton.test.jsx
+++ src/components/LikeButton/__test__/LikeButton.test.jsx
@@ -4,7 +4,13 @@ import LikeButton from '../LikeButton';
 
 describe(LikeButton.name, () => {
   it('should match the snapshot', () => {
-    const { container } = render(<LikeButton />);
+    const { container } = render(<LikeButton
+      currentState
+      likes={1}
+      id="abc"
+      patchLike={jest.fn()}
+      updateSongs={jest.fn()}
+    />);
     expect(container).toMatchSnapshot();
   });
 });
