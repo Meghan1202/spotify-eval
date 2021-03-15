c2fc0acd4c5e03b0809ca22e578785c37a02717f
Meghana Khuntia <meghanakhuntia12@gmail.com>
2021-03-15 17:35:26 +0530

    fix: add test cases for LandingPage

--- src/pages/LandingPage/__test__/LandingPage.test.jsx
+++ src/pages/LandingPage/__test__/LandingPage.test.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import LandingPage from '../LandingPage';
 
 describe(LandingPage.name, () => {
   it('should match the snapshot', () => {
-    const { container } = render(<LandingPage />);
+    const { container } = render(
+      <BrowserRouter>
+        <LandingPage getSongs={jest.fn()} />
+      </BrowserRouter>,
+    );
     expect(container).toMatchSnapshot();
   });
 });
